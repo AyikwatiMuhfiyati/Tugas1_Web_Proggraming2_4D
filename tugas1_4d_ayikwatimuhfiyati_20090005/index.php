541b127e5a4c9d8161a598cdf99aa3004a6566bd
AyikwatiMuhfiyati <80374601+AyikwatiMuhfiyati@users.noreply.github.com>
2022-03-02 12:32:29 +0700

    Add files via upload

---
+++ tugas1_4d_ayikwatimuhfiyati_20090005/index.php
@@ -0,0 +1,4 @@
+<h1>Halo, Assalamu'alaikum</h1>
+
+Kenalan Yuk! Klik Link dibawah! <br/>
+<a href="test.php"> Klik Disini! </a>
---
+++ tugas1_4d_ayikwatimuhfiyati_20090005/test.php
@@ -0,0 +1,22 @@
+<?php
+
+#for($i=1; $i<=10; $i++){
+# print("Urutan ke-$i\n");
+#}
+
+$nim = "20090005";
+$nama = "Ayikwati Muhfiyati";
+$kelas = "4D Teknik Informatika";
+echo 'Halo, Selamat Datang !<br/>';
+echo "Perkenalkan <br/>
+Nama Saya : $nama <br/>
+NIM Saya : $nim <br/>
+Kelas : $kelas <br/>";
+
+#$output = shell_exec("ls -lart");
+#echo $output;
+#shell_exec("touch index.php");
+#shell_exec("echo '<h1>Hai, Apa kabar</h1>' > index.php");
+print("Salam Kenal dari Saya $nama\n");
+  
+?>
